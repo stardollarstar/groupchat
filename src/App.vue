92f73a96a454162f5d6fbeb7a82082c0538a0a19
stardollarstar <stardollarstar@gmail.com>
2020-10-30 18:00:22 +0800

    src/components/ 	src/maincss.css 	src/profile.jpg 	src/user1.jpg

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Side style="min-height: 100vh;"/>
+    <Chat style="min-height: 100vh;"/>
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Side from './components/Sidebar'
+import Chat from './components/Chat'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Side,
+    Chat
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+  }
 </style>
